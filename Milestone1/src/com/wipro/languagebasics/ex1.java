bda7c698cf4fe8cefc63e1c3a4b0ccc82d1b13a5
Dell <Dell@DESKTOP-V7V4SME>
2020-06-13 15:02:41 +0530

    example 1 updated

--- Milestone1/src/com/wipro/languagebasics/ex1.java
+++ Milestone1/src/com/wipro/languagebasics/ex1.java
@@ -1,5 +1,13 @@
 package com.wipro.languagebasics;
 
 public class ex1 {
+	public static void main(String args[])
 
+	 {
+
+	  System.out.print(args[0] + " technologies ");
+
+	  System.out.print(args[1]);
+
+	   }
 }
